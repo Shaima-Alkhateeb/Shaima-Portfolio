ec068bd187be47594f6f719cb367395d3a961807
Shaima Alkhateeb <shaimaalkhateeb535@gmail.com>
2022-07-31 23:17:02 +0300

    add day06 branch

---
+++ city-explorer1/src/App.js
@@ -0,0 +1,39 @@
+// import logo from './logo.svg';
+import './App.css';
+// import axios from 'axios';
+import { Component } from 'react';
+
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     pokemon: []
+  //   }
+  // }
+  // getPokemon = async() => {
+  //   const pokemonData = await axios.get(`${process.env.REACT_APP_MAIN_URL}/pokemon`)
+  //   // console.log(pokemon.data.results);
+  //   this.setState({
+  //     pokemon: pokemonData.data.results
+  //   });
+  //   console.log(pokemonData)
+  // }
+  
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <h1>{process.env.REACT_APP_TITLE}</h1>
+  //       <button onClick={this.getPokemon}>submit</button>
+  //       {this.state.pokemon && this.state.pokemon.map(item => {
+  //         return (
+  //           <li>{item.name}</li>
+  //         )
+  //       })
+
+  //       }
+  //     </div>
+  //   );
+  // }
+}
+
+export default App;
